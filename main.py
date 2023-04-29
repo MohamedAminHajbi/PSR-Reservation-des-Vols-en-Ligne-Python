d8b4d3928be1fa3d2ebc7478c26cf773d9980124
unknown <aminhajbi116@gmail.com>
2023-04-29 17:18:07 +0100

    gestion de fichiers de vols - code finale

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from vols_manager import GestionnaireVols
-from vols_manager import Histo
+
 
 # Create a new instance of GestionnaireVols with the name of the input file
-gestionnaire_vols = GestionnaireVols("vols.txt")
+gestionnaire_vols = GestionnaireVols()
 
 
-# Cancel a reservation on a flight
-cancellation_successful, message = gestionnaire_vols.reserver("001", "Agence1", "Annulation", 2)
+# Reserve some seats on a flight
+reservation_successful, message = gestionnaire_vols.reserver("001", "Agence2", "Annulation", 5)
 print(message)
 
-# Generate the list of invoices for the agencies
-gestionnaire_vols.generer_factures("factures.txt")
