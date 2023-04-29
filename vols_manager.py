2417b1e1756886817f57560aebc83e8f513588e2
adam <adamsahli102@gmail.com>
2023-04-29 17:04:50 +0100

    serveur/client

--- vols_manager.py
+++ vols_manager.py
@@ -1,18 +1,30 @@
+#class vols
+
 class Vol:
     def __init__(self, ref_vol, destination, nb_places_dispo, prix_place):
         self.ref_vol = ref_vol
         self.destination = destination
         self.nb_places_dispo = nb_places_dispo
         self.prix_place = prix_place
+        
+        
+#calss gestion des vols   
+        
 class GestionnaireVols:
+    
+    
+    
     def __init__(self, nom_fichier):
         self.vols = {}
         self.factures = {}
+        self.transactions = []
         with open(nom_fichier, 'r') as f:
             for line in f:
                 ref_vol, destination, nb_places_dispo, prix_place = line.strip().split()
                 self.vols[ref_vol] = Vol(ref_vol, destination, int(nb_places_dispo), float(prix_place))
         f.close()
+
+
 
     def sauvegarder_vols(self, nom_fichier):
         with open(nom_fichier, 'w') as f:
@@ -20,13 +32,42 @@ class GestionnaireVols:
                 f.write(f"{vol.ref_vol} {vol.destination} {vol.nb_places_dispo} {vol.prix_place}\n")
         f.close()
 
-    def reserver(self, ref_vol, agence, transaction, valeur):
+
+
+   
+        
+    def sauvegarder_histo(self, nom_fichier):
+       
+        with open(nom_fichier, 'w') as f:
+            f.write("Référence Vol Agence Transaction Valeur Résultat\n")
+            for transaction in self.transactions:
+                ref_vol, agence, transaction_type, valeur, resultat = transaction
+                f.write(f"{ref_vol} {agence} {transaction_type} {valeur} {resultat}\n")
+        f.close()
+        
+    def ajouter_transaction(self, ref_vol, agence, transaction, valeur, resultat):
+        with open(nom_fichier, 'r') as f:
+            for line in f:
+                ref_vol, agence, transaction, valeur, resultat = line.strip().split()
+                self.transactions.append((ref_vol, agence, transaction, int(valeur), resultat))
+        f.close()
+        self.transactions.append((ref_vol, agence, transaction, valeur, resultat))
+        if resultat == "Demande acceptée" or resultat == "Annulation acceptée":
+            GestionnaireVols.factures[agence] = GestionnaireVols.factures.get(agence, 0)
+            
+            
+            
+            
+            
+    def reserver(self, ref_vol, agence, transaction, valeur ,resultat):
         vol = self.vols.get(ref_vol)
         if vol is None:
             return False, "Vol inexistant"
         if transaction == "Demande":
             if vol.nb_places_dispo >= valeur:
                 vol.nb_places_dispo -= valeur
+                resultat = "Demande acceptée"
+                ajouter_transaction(self, ref_vol, agence, transaction, valeur, resultat)
                 montant = valeur * vol.prix_place
                 self.factures[agence] = self.factures.get(agence, 0) + montant
                 # Update vols.txt with new information
@@ -39,7 +80,7 @@ class GestionnaireVols:
                             nb_places = str(vol.nb_places_dispo)
                             prix = str(vol.prix_place)
                         f.write(f"{ref} {dest} {nb_places} {prix}\n")
-                return True, "Demande acceptée"
+                return True, resultat
             else:
                 return False, "Demande refusée"
         elif transaction == "Annulation":
@@ -60,6 +101,8 @@ class GestionnaireVols:
             return True, "Annulation acceptée"
         else:
             return False, "Transaction invalide"
+        
+        
     def generer_factures(self, nom_fichier):
         with open(nom_fichier, 'w') as f:
             for agence, montant in self.factures.items():
@@ -71,8 +114,18 @@ class GestionnaireVols:
             for agence, montant in self.factures.items():
                 f.write(f"{agence} {montant}\n")
         f.close()
+        
+        
+        
+        
+        
+        
+        
+        
+  #class histo      
 
 class Histo:
+    
     def __init__(self, nom_fichier):
         self.transactions = []
         with open(nom_fichier, 'r') as f:
@@ -85,6 +138,7 @@ class Histo:
         self.transactions.append((ref_vol, agence, transaction, valeur, resultat))
         if resultat == "Demande acceptée" or resultat == "Annulation acceptée":
             GestionnaireVols.factures[agence] = GestionnaireVols.factures.get(agence, 0)
+            
 
     def sauvegarder_histo(self, nom_fichier):
         with open(nom_fichier, 'w') as f:
